6aba15697c0aec1870a939bc220016afe82d4d18
Ray <rayco020@hotmail.com>
2021-09-01 17:08:28 +0200

    daytwo

--- simul_make.py
+++ simul_make.py
@@ -31,3 +31,28 @@ bgColor = BLACK
 
 XMARGIN = int((WINDOWWIDTH - (2 * BUTTONSIZE) - BUTTONGAP) /2)
 YMARGIN = int((WINDOWHIGHT - (2 * BUTTONSIZE) - BUTTONGAP) /2)
+
+# Rect objects for each of the four buttons
+YELLOWRECT = pygame.Rect(XMARGIN,YMARGIN, BUTTONSIZE, BUTTONSIZE)
+BLUERECT = pygame.Rect(XMARGIN + BUTTONSIZE + BUTTONGAP, YMARGIN, BUTTONSIZE, BUTTONSIZE)
+REDRECT = pygame.Rect(XMARGIN, YMARGIN + BUTTONSIZE + BUTTONGAP, BUTTONSIZE, BUTTONSIZE)
+GREENRECT = pygame.Rect(XMARGIN + BUTTONSIZE + BUTTONGAP, YMARGIN + BUTTONSIZE + BUTTONGAP, BUTTONSIZE, BUTTONSIZE )
+
+def main():
+    global FPSCLOCK, DISPLAYSURF, BASICFONT, BEEP1, BEEP2, BEEP3, BEEP4
+
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHIGHT))
+    pygame.display.set_caption('Simulate')
+
+    BASICFONT = pygame.font.Font('freesansbold.ttf',16)
+    
+    infoSurf = BASICFONT.render('Match the pattern by clicking on the button or using the Q, W, A, S keys.', 1, DARKGRAY)
+    infoRect = infoSurf.get_rect()
+    infoRect.topleft = (10, WINDOWHIGHT -25)
+    # load the sound files
+    BEEP1 = pygame.mixer.Sound('beep1.org')
+    BEEP2 = pygame.mixer.Sound('beep2.org')
+    BEEP3 = pygame.mixer.Sound('beep3.org')
+    BEEP4 = pygame.mixer.Sound('beep4.org')
