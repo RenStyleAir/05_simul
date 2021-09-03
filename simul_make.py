283f4e5ad7cf40bb71b50f9054aad261cd3a8283
Ray <rayco020@hotmail.com>
2021-09-03 17:15:10 +0200

    daythree

--- simul_make.py
+++ simul_make.py
@@ -95,7 +95,7 @@ def main():
 
 
         if not waitingForInput:
-            # paly the pattern
+            # play the pattern
             pygame.display.update()
             pygame.time.wait(1000)
             pattern.append(random.choice(YELLOW, BLUE, RED, GREEN))
@@ -103,11 +103,62 @@ def main():
                 flashButtonAnimation(button)
                 pygame.time.wait(FLASHDELAY)
             waitingForInput = True
+        else:
+            #
+            if clickedButton and clickedButton == pattern[currentStep]:
+                #
+                flashButtonAnimation(clickedButton)
+                currentStep += 1
+                lastClickTime = time.time()
+
+                if currentStep == len(pattern):
+                    #
+                    changeBackgroundAnimation()
+                    score += 1
+                    waitingForInput = False
+                    currentStep = 0
+
+                elif (clickedButton and clickedButton != pattern[currentStep]) or (currentStep != 0 and time.time() - TIMEOUT > lastClickTime ):
+                    #
+                    gameOverAnimation()
+                    #
+                    pattern = []
+                    currentStep = 0 
+                    waitingForInput = False
+                    score = 0 
+                    pygame.time.wait(1000)
+                    changeBackgroundAnimation()
+
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def checkForQuit():
+    for event in pygame.event.get(QUIT):
+        terminate()
+    for event in pygame.event.get(KEYUP):
+        if event.key == K_ESCAPE:
+            terminate()
+        pygame.event.post(event)
+    
+
+    
+
+
+
+
+
+    
                 
 
 
 
 
-    # 18:25
+    # 16.42
 
 
